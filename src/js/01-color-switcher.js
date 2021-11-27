46901e5c48c920201a6ff4374d241228839a7b0a
BlockCHYIN <anatoliimarkovskii@gmail.com>
2021-11-27 21:09:10 +0200

    add hw/9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,18 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+let timerId = null;
+btnStart.disabled = false;
 
+const setBackground = () => {
+    timerId = setInterval(() => {
+        const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        document.body.style.background = "#" + randomColor;
+    }, 1000);
+    btnStart.disabled = true;
+}
+
+btnStart.addEventListener('click', setBackground);
+btnStop.addEventListener('click', () => {
+    clearInterval(timerId);
+    btnStart.disabled = false;
+});
